91b0680df2b4c7f42fe8cf08401805ec7a8e5bda
shinseongcheol <cher3725@naver.com>
2025-04-11 00:51:26 +0900

    Feat "Candler Scheduler"
    - 일별 캔들 조회 데이터 조회 및 추가

--- backend/src/main/java/com/seongcheol/coin_collection/scheduler/CandleScheduler.java
+++ backend/src/main/java/com/seongcheol/coin_collection/scheduler/CandleScheduler.java
@@ -2,17 +2,21 @@ package com.seongcheol.coin_collection.scheduler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.seongcheol.coin_collection.service.CandleService;
 
 @Component
 public class CandleScheduler {
 	
-	Logger logger = LoggerFactory.getLogger(this.getClass());
-
-	@Scheduled(cron = "*/10 * * * * *")
+	@Autowired
+	private CandleService candleService;
+	
+	@Scheduled(cron = "10 0 0 * * *")
 	public void run() {
-		logger.info("run");
+		candleService.getDayCandle();
 	}
 	
 }

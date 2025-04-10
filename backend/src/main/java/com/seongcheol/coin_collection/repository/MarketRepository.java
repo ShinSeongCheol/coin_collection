d1fd406c44d0261941da7d4603a6e1873220f694
shinseongcheol <cher3725@naver.com>
2025-04-11 00:11:47 +0900

    Refactor "Marker Repository"

--- backend/src/main/java/com/seongcheol/coin_collection/repository/MarketRepository.java
+++ backend/src/main/java/com/seongcheol/coin_collection/repository/MarketRepository.java
@@ -1,6 +1,5 @@
 package com.seongcheol.coin_collection.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.seongcheol.coin_collection.domain.Market;
 
 public interface MarketRepository extends JpaRepository<Market, Integer>{
-	List<Market> findAll();
 	Optional<Market> findByMarket(String market);
 	boolean existsByMarket(String market);
 }
